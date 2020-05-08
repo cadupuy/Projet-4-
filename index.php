f7a482698ef80d4a857211b2e2f8e4eb2b49301a
Charles-Antoine Dupuy <charles-antoine.dupuy@orange.fr>
2020-05-08 16:55:56 +0200

    Création d'un fichier modèle pour préparer à l'architecture MVC et utilisation de la page index comme un pont entre la page vue et la page  modèle

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
-// Accès aux données
-$bdd = new PDO('mysql:host=localhost;dbname=projet 4;charset=utf8', 'root', 'root');
-$billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
-  . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
-  . ' order by BIL_ID desc');
 
-// Affichage
+require 'Modele.php';
+
+$billets = getBillets();
+
 require 'vueAccueil.php';
