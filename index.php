76a2205d9e9fb1e2da962a68545ccd29b3c39089
Charles-Antoine Dupuy <charles-antoine.dupuy@orange.fr>
2020-05-08 16:37:34 +0200

    Création de la connexion à la base de données

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-    </head>
-    <body>
+<?php
+// Accès aux données
+$bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8', 'root', 'root');
+$billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
+  . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
+  . ' order by BIL_ID desc');
 
-    </body>
-</html>
+// Affichage
+require 'vueAccueil.php';
